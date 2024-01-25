6a9cfcaea9f1852019f5ca13f6f5f5e96083b64a
Nolen Royalty <nolen.royalty@gmail.com>
2024-01-24 19:59:39 -0500

    initial setup + mobile header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { COLORS, WEIGHTS } from '../../constants';
-import Logo from '../Logo';
-import SuperHeader from '../SuperHeader';
-import MobileMenu from '../MobileMenu';
+import { COLORS, WEIGHTS, QUERIES } from "../../constants";
+import Logo from "../Logo";
+import SuperHeader from "../SuperHeader";
+import MobileMenu from "../MobileMenu";
+import Icon from "../Icon";
 
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
@@ -16,7 +17,9 @@ const Header = () => {
 
   return (
     <header>
-      <SuperHeader />
+      <SuperHeaderWrapper>
+        <SuperHeader />
+      </SuperHeaderWrapper>
       <MainHeader>
         <Side>
           <Logo />
@@ -29,7 +32,16 @@ const Header = () => {
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
         </Nav>
-        <Side />
+
+        <Side>
+          <MobileMenuWrapper>
+            <Icon id="shopping-bag" size={24} strokeWidth={2} />
+            <Icon id="search" size={24} strokeWidth={2} />
+            <Button onClick={() => setShowMobileMenu(true)}>
+              <Icon id="menu" size={24} strokeWidth={2} />
+            </Button>
+          </MobileMenuWrapper>
+        </Side>
       </MainHeader>
 
       <MobileMenu
@@ -40,22 +52,62 @@ const Header = () => {
   );
 };
 
+const Button = styled.button`
+  background: transparent;
+  border: none;
+  display: flex;
+  padding: 0;
+`;
+
+const MobileMenuWrapper = styled.div`
+  display: none;
+
+  @media ${QUERIES.tabletAndDown} {
+    gap: 16px;
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const SuperHeaderWrapper = styled.div`
+  @media ${QUERIES.tabletAndDown} {
+    display: none;
+  }
+`;
+
 const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  /* justify-content: flex-end; */
+  /* justify-content: space-between; */
+
+  @media ${QUERIES.tabletAndDown} {
+    border-top: 4px solid ${COLORS.gray[900]};
+    justify-content: space-between;
+    padding: 18px 16px;
+    align-items: center;
+  }
 `;
 
 const Nav = styled.nav`
   display: flex;
   gap: 48px;
   margin: 0px 48px;
+
+  @media ${QUERIES.tabletAndDown} {
+    display: none;
+  }
 `;
 
 const Side = styled.div`
   flex: 1;
+
+  @media ${QUERIES.tabletAndDown} {
+    flex: 0;
+  }
 `;
 
 const NavLink = styled.a`
